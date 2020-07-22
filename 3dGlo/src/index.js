93a6240fbd6b2bc1078af0e9fcffcbb4462fcd76
Pavel Baksheev <sxeiter@mail.ru>
2020-07-22 11:57:02 +0500

    lesson30

--- 3dGlo/src/index.js
+++ 3dGlo/src/index.js
@@ -1,3 +1,8 @@
+'use strict';
+import '@babel/polyfill';
+import 'nodelist-foreach-polyfill';
+
+
 import sendForm from './modules/sendform';
 import calc from './modules/calc';
 import deleteword from './modules/deleteword';
@@ -7,4 +12,15 @@ import tabs from './modules/tabs';
 import popup from './modules/popup';
 import toggleMenu from './modules/toggleMenu';
 import timer from './modules/timer';
+import countTimer from './modules/timer';
 
+countTimer();
+toggleMenu();
+timer();
+popup();
+tabs();
+slider();
+changephoto();
+deleteword();
+calc();
+sendForm();
---
+++ 3dGlo/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, '.dist'),
+        filename: '[main].js',
+        publicPath: '/dist'
+    },
+    devServer: {
+        overlay: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: 'node_modules'
+            }
+        ]
+    }
+};
